3b4d9b5df13c86096ad5b8412b48321b815c3b3f
jeff <jeanfrancois.roussel@corum-am.com>
2021-02-13 19:48:20 +0100

    Updates

---
+++ precache-manifest.fbeea1fa610810beb97c0dbc53bfdbaa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "870f0cfeb05ba2909efcd3d70675964e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c06fe1bca86bc195ff62",
+    "url": "./static/css/main.cbb29a1b.chunk.css"
+  },
+  {
+    "revision": "bc78d94955fa118bf463",
+    "url": "./static/js/2.010f471d.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.010f471d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c06fe1bca86bc195ff62",
+    "url": "./static/js/main.cc818e87.chunk.js"
+  },
+  {
+    "revision": "2e63dc2c8e56db6e1cfe",
+    "url": "./static/js/runtime-main.c67d52b0.js"
+  }
+]);
